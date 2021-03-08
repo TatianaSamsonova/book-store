62410d1c851e1e00db612a6dd85c166bef14e11f
TatianaSamsonova <tatiana.samsonova.dev@gmail.com>
2021-03-08 17:19:41 +0200

    Add origin link in the @CrossOrigin annotation for Angular calls

--- src/main/java/sam/ecommerce/bookstore/controller/UserController.java
+++ src/main/java/sam/ecommerce/bookstore/controller/UserController.java
@@ -1,6 +1,7 @@
 package sam.ecommerce.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import sam.ecommerce.bookstore.facade.UserFacade;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping( "api/users")
 public class UserController {
     @Autowired
