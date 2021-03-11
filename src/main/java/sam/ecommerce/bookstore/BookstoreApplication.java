213757d7b498dddb955806a05164dfccae5aec71
TatianaSamsonova <tatiana.samsonova.dev@gmail.com>
2021-03-11 15:29:45 +0200

    Configure swagger

--- src/main/java/sam/ecommerce/bookstore/BookstoreApplication.java
+++ src/main/java/sam/ecommerce/bookstore/BookstoreApplication.java
@@ -3,7 +3,10 @@ package sam.ecommerce.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +21,22 @@ public class BookstoreApplication {
 
     @Bean
     public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("sam.ecommerce.bookstore")).build();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("sam.ecommerce.bookstore"))
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Tatiana Samsonova", "https://github.com/TatianaSamsonova", "tatiana.samsonova.dev@gmail.com");
+        return new ApiInfoBuilder()
+                .title("BookStore Project API")
+                .description("Documentation for E-commerce BookStore project api")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
     }
 }
